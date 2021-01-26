9d44a74236e9f19b5a6690133bc9b8a9cd189198
Rich Kadel <richkadel@google.com>
2021-01-25 19:46:13 -0800

    Added more error checking and output tab logging, and updated the readme.

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-# FuchsiAware - Fuchsia OS Source Tree Browsing Assistance
+# FuchsiAware
+## Fuchsia OS Source Tree Browsing Assistance
 
-This extension recognizes Fuchsia-specific artifacts in the Fuchsia Git-based source repository and
-adds links and references to help navigate them.
+This extension recognizes Fuchsia-specific artifacts in the Fuchsia Git-based source repository
+(also known as `fuchsia.git`) and adds links and references to help navigate them.
 
 * Converts fuchsia component URLs (beginning with the `fuchsia-pkg` scheme), in any file, to links
   that open the component's manifest source (either `.cml` or `.cmx`).
@@ -12,6 +13,22 @@ adds links and references to help navigate them.
   Right-click a file and select menu option "Go to References" or "Find References".
 
 ![example-references](images/preview-references.png)
+
+## Minimum Requirements and Implementation Details
+
+* You can run the `git` command.
+* Your VS Code workspace has exactly one workspace folder that is the root of your cloned `fuchsia`
+  repo.
+* You ran `fx set ...` (or `fx use ...`) to set the current build directory, and generate _Ninja_
+  build dependencies.
+
+The extension reads build commands from the `toolchain.ninja` file in your build directory to
+determine the package and component names assocated with each manifest (to establish links from
+matching component URLs), and uses the `git grep` command to find references from any text document
+in the `fuchsia.git` repo to a known component URL.
+
+Note that a component URL is linked only if its manifest is included in the current `fx set ...`
+build configuration.
 
 ## Extension Settings
 
--- src/extension.ts
+++ src/extension.ts
@@ -2,12 +2,26 @@ import * as vscode from 'vscode';
 import * as child_process from 'child_process';
 import * as fs from 'fs';
 import * as readline from 'readline';
-import * as stream from 'stream';
-import { resolveCliPathFromVSCodeExecutablePath } from 'vscode-test';
+
+const output = vscode.window.createOutputChannel('FuchsiAware');
+
+function info(message: string) {
+  output.appendLine(`INFO (fuchsiaware): ${message}`);
+}
+
+function warn(message: string) {
+  output.appendLine(`WARNING (fuchsiaware): ${message}`);
+  vscode.window.showWarningMessage(message);
+}
+
+function error(message: string) {
+  output.appendLine(`ERROR (fuchsiaware): ${message}`);
+  vscode.window.showErrorMessage(message);
+}
 
 // this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) {
-  console.log('fuchsia package-to-source links extension is activated');
+  info('fuchsia package-to-source links extension is activated');
   const provider = new Provider();
   provider.init().then(() => {
     context.subscriptions.push(vscode.Disposable.from(
@@ -54,12 +68,32 @@ export class Provider implements vscode.DocumentLinkProvider, vscode.ReferencePr
   }
 
   private async _findFuchsiaBuildDir(): Promise<[vscode.Uri, string] | undefined> {
-    for (const folder of (vscode.workspace.workspaceFolders || [])) {
-      const buildDirUri = folder.uri.with({ path: folder.uri.path + '/.fx-build-dir' });
-      const buildDirDoc = await vscode.workspace.openTextDocument(buildDirUri);
-      const buildDir = buildDirDoc.getText().trim();
-      return [folder.uri, buildDir];
+    const buildDirFilename = '.fx-build-dir';
+    if (!vscode.workspace.workspaceFolders) {
+      return;
     }
+    for (const folder of (vscode.workspace.workspaceFolders || [])) {
+      const buildDirUri = folder.uri.with({ path: `${folder.uri.path}/${buildDirFilename}` });
+      const buildDirDoc = await vscode.workspace.openTextDocument(buildDirUri);
+      if (buildDirDoc) {
+        const buildDir = buildDirDoc.getText().trim();
+        info(
+          `Workspace folder '${folder.uri.fsPath}' contains the '${buildDirFilename}' file, which ` +
+          `specifies that the current Fuchsia build directory is '${buildDir}'`
+        );
+        return [folder.uri, buildDir];
+      } else {
+        info(
+          `Workspace folder '${folder.uri.fsPath}' does not contain a file named '${buildDirFilename}'`
+        );
+      }
+    }
+    warn(
+      `Could not find file '${buildDirFilename}' in the root of any open VS Code workspace. ` +
+      `If you have an open workspace for the 'fuchsia.git' source tree, make sure the ` +
+      `workspace is rooted at the repository root directory (e.g., 'fuchsia'), and ` +
+      'run `fx set ...` or `fx use ...`, then reload the VS Code window.'
+    );
     return;
   }
 
@@ -83,7 +117,15 @@ export class Provider implements vscode.DocumentLinkProvider, vscode.ReferencePr
 
     const ninjaTargetsUri = baseUri.with({ path: `${baseUri.path}/${buildDir}/toolchain.ninja` });
     const ninjaStream = fs.createReadStream(ninjaTargetsUri.fsPath);
+    ninjaStream.on('error', (err) => {
+      error(
+        `Error reading the build dependencies from ${ninjaTargetsUri.fsPath}: '${err}'\n` +
+        'You may need to re-run `fx set ...` and then reload your VS Code window.'
+      );
+    });
     const ninjaReadline = readline.createInterface(ninjaStream);
+
+    let loggedAtLeastOneExample = false;
 
     for await (const line of ninjaReadline) {
       let match;
@@ -105,6 +147,23 @@ export class Provider implements vscode.DocumentLinkProvider, vscode.ReferencePr
       const packageAndComponent = `${packageName}/${componentName}`;
       packageAndComponentToSource.set(packageAndComponent, sourceUri);
       sourceToPackageAndComponent.set(sourceUri.fsPath, packageAndComponent);
+      if (!loggedAtLeastOneExample) {
+        loggedAtLeastOneExample = true;
+        info(
+          `Getting links to manifests, based on build dependencies in ${ninjaTargetsUri.fsPath}, ` +
+          `for example, '${sourceUri.fsPath}' is the manifest source for package '${packageName}', ` +
+          `component '${componentName}'.`
+        );
+      }
+    }
+
+    if (!loggedAtLeastOneExample) {
+      error(
+        `None of the build dependency commands in '${ninjaTargetsUri.fsPath}' matched expected ` +
+        `patterns associating a package and component to its manifest source:\n\n` +
+        `  cmlRegEx = /${cmlRegEx}/\n` +
+        `  cmxRegEx = /${cmxRegEx}/`
+      );
     }
 
     return [packageAndComponentToSource, sourceToPackageAndComponent];
@@ -116,28 +175,53 @@ export class Provider implements vscode.DocumentLinkProvider, vscode.ReferencePr
   ): Promise<Map<string, vscode.Location[]>> {
     const packageAndComponentToReferences = new Map<string, vscode.Location[]>();
 
+    const gitArgs = [
+      '--no-pager',
+      'grep',
+      '--recurse-submodules',
+      '-I',
+      '--extended-regexp',
+      '--line-number',
+      // '--column',
+      // '--only-matching', // grep BUG! --column value is wrong for second match in line
+      '--no-column',
+      '--no-color',
+      'fuchsia-pkg://fuchsia.com/([^#]*)#meta/(-|\\w)*\\.cmx?',
+    ];
+
+    info(
+      `Searching for component URLs ('fuchsia-pkg://...cm[x]') referenced from any text document ` +
+      `in the 'fuchsia.git' repo, by running the command:\n\n` +
+      `  \`git ${gitArgs.join(' ')}\`\n\n` +
+      `from the '${baseUri.path}' directory.`
+    );
+
     let gitGrep = child_process.spawnSync(
       'git',
-      [
-        '--no-pager',
-        'grep',
-        '--recurse-submodules',
-        '-I',
-        '--extended-regexp',
-        '--line-number',
-        // '--column',
-        // '--only-matching', // grep BUG! --column value is wrong for second match in line
-        '--no-column',
-        '--no-color',
-        'fuchsia.pkg://fuchsia.com/([^#]*)#meta/(-|\\w)*\\.cmx?',
-      ],
+      gitArgs,
       { cwd: `${baseUri.path}` }
     );
+
+    if (gitGrep.error) {
+      error(
+        `Error executing the \`git grep\` command: '${gitGrep.error}'\n`
+      );
+      return packageAndComponentToReferences; // return an empty map
+    }
+
+    if (gitGrep.status !== 0) {
+      error(
+        `Error (${gitGrep.status}) executing the \`git grep\` command: '${gitGrep.stderr}'\n`
+      );
+      return packageAndComponentToReferences; // return an empty map
+    }
 
     const text = gitGrep.stdout.toString();
 
     // patterns end in either '.cm' or '.cmx'
     const urlRegEx = /\bfuchsia-pkg:\/\/fuchsia.com\/([-\w]+)(?:\?[^#]*)?#meta\/([-\w]+).cmx?\b/g;
+
+    let loggedAtLeastOneExample = false;
 
     let start = 0;
     while (start < text.length) {
@@ -164,12 +248,32 @@ export class Provider implements vscode.DocumentLinkProvider, vscode.ReferencePr
           lineNumber,
           column + componentUrl.length,
         );
+        const location = new vscode.Location(sourceUri, range);
         let references = packageAndComponentToReferences.get(packageAndComponent);
         if (!references) {
           references = [];
           packageAndComponentToReferences.set(packageAndComponent, references);
         }
-        references.push(new vscode.Location(sourceUri, range));
+        if (!loggedAtLeastOneExample) {
+          loggedAtLeastOneExample = true;
+          info(
+            `Getting references to manifests. For example, '${componentUrl}' is referenced by ` +
+            `'${location.uri.fsPath}:` +
+            `${location.range.start.line + 1}:` +
+            `${location.range.start.character + 1}:` +
+            `${location.range.end.line + 1}:` +
+            `${location.range.end.character + 1}`
+          );
+        }
+        references.push(location);
+      }
+      if (!loggedAtLeastOneExample) {
+        loggedAtLeastOneExample = true;
+        warn(
+          `Regex failed to match the first line returned from \`git grep\`.\n\n` +
+          `  Line: '${matchedLine}'\n` +
+          `  Regex: /${urlRegEx}/`
+        );
       }
     }
 
